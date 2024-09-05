13e945bbc9b244f8675a19333fb29c5bb675fbf8
mohamedemadal <malshiekh41@gmail.com>
2024-09-05 04:00:08 -0700

    first

---
+++ dist/assets/VaPageNotFoundLargeText-21ec8770.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./VaPageNotFound.vue_vue_type_style_index_0_lang-dfe83fe0.js";import{z as a,e as t,C as o,w as s,h as _}from"./index-9b6dcbe1.js";const n=_("div",{class:"va-page-not-found-large-text__number"},"404",-1),d=a({setup(r){return(c,p)=>(t(),o(e,{class:"va-page-not-found-large-text"},{image:s(()=>[n]),_:1}))}});export{d as default};
